8e31189693115f836d168d49f51edeb4e078002d
gvnmcg <gvnmcg517@gmail.com>
2022-11-23 01:03:53 -0700

    draw chords on canvas

--- pages/chords-app/FretboardCanvas.jsx
+++ pages/chords-app/FretboardCanvas.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const noteNames = [
   "C",
@@ -16,9 +16,15 @@ const noteNames = [
 ];
 
 const scaleNumbers = [1, 0, 2, 0, 3, 4, 0, 5, 0, 6, 0, 7, 1];
+ const initChordNote = {fret:0, str:0};
+
 
 const FretboardCanvas = ({ tuning, scale }) => {
+  let [chordNote, setChordNote] = useState(initChordNote);
+  let [chordSet, setChordSet] = useState([initChordNote])
+  
   const canvasRef = useRef(null);
+  // let rect = undefined;
 
   const drawStringNotes = (ctx) => {
     // draw background
@@ -46,24 +52,58 @@ const FretboardCanvas = ({ tuning, scale }) => {
           ctx.fill();
 
           ctx.fillStyle = "#FF5733";
-          ctx.fillText(noteName, x, y);
+          ctx.fillText(noteName, x- 5, y+5);
         }
       }
     }
   };
 
+  const drawChordNotes = (ctx, rect) => {
+    ctx.fillStyle = "#BADA55";
+
+    chordSet.forEach(cn => {
+      let x = cn.fret - rect.left;
+      x = Math.floor(x/30)* 30;
+      let y = cn.str - rect.top;
+      y = Math.floor(y/30)* 30;
+
+      ctx.beginPath();
+      ctx.arc(x, y, 10, 0, 2 * Math.PI);
+      ctx.fill();
+    })
+
+  }
+
+  const updateChord = (e) => {
+    let x = e.clientX ;
+    let y = e.clientY ;
+    // let str = y / 30;
+    // let open = tuning[str]
+    // let fret = x/30;
+    let newNote ={str:y, fret:x}
+    setChordNote(newNote)
+    setChordSet([...chordSet, newNote])
+    console.table(x,y)
+  }
+
   useEffect(() => {
     console.log("redraw");
     const canvas = canvasRef.current;
+    let rect = canvas.getBoundingClientRect()
+
     const context = canvas.getContext("2d");
     drawStringNotes(context);
-  }, [tuning, scale]);
+   
+    drawChordNotes(context, rect);
+  }
+  // , [tuning, scale]
+  );
 
   return <canvas 
       ref={canvasRef} 
       width="500" 
       height="300" 
-      onClick={(e) => {onMouseOverFretboard(e)}}
+      onClick={(e) => {updateChord(e)}}
       
     />;
 };
